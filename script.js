28279e36b74c506de0cd467528de0d8d760681be
davidnouchi <david.nouchi@latoile.em-lyon.com>
2025-12-16 11:12:28 +0100

    Premier push

---
+++ script.js
@@ -0,0 +1,6 @@
+// Pseudo-Code :
+// 1) Créer des sections avec des ID dans le HTML
+// 2) Récupérer les ID de ces sections avec getelementByID dans le JS
+// 3) Récupérer le fichier json avec son url à l'aide de la méthode fetch()
+// 4) console.log(data.produits) pour vérifier si on a bien récupéré les données
+// 5) 
