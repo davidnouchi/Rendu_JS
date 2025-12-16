e8ca94ad4e5a7dc01bf15c977e6b2a90cf6b9bbc
davidnouchi <david.nouchi@latoile.em-lyon.com>
2025-12-16 12:28:38 +0100

    Début du HTML et du pseudo-code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Pseudo-Code :
-// 1) Créer des sections avec des ID dans le HTML
+// 1) Créer des sections avec des ID dans le HTML dont une section "hero"
 // 2) Récupérer les ID de ces sections avec getelementByID dans le JS
 // 3) Récupérer le fichier json avec son url à l'aide de la méthode fetch()
 // 4) console.log(data.produits) pour vérifier si on a bien récupéré les données
-// 5) 
+// 5) for each pour chaque objet du tableau data.produits pour récupérer "nom" puis "description" et "image-url"
+// 6) Créer les éléments HTML
+// 6.a) Créer une div pour accueillir tous les éléments de notre card
+// 6.b) Créer les éléments HTML suivants dans un inner.html :
+// 
